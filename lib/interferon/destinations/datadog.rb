94a7cfb9c4ffeb6dd7e1b40dea070a3f9feb272c
Jimmy Ngo <jingo@linkedin.com>
2016-07-14 16:29:12 -0700

    Move datadog query syntax checking to alerts repo

--- lib/interferon/destinations/datadog.rb
+++ lib/interferon/destinations/datadog.rb
@@ -1,9 +1,6 @@
 require 'diffy'
 require 'dogapi'
 require 'set'
-require 'treetop'
-
-Treetop.load 'datadog.treetop'
 
 Diffy::Diff.default_format = :text
 
@@ -56,8 +53,6 @@ module Interferon::Destinations
         :api_unknown_errors => 0,
         :manually_created_alerts => 0,
       }
-
-      @datadog_query_parser = DatadogQueryParser.new
     end
 
     def api_errors
@@ -130,10 +125,6 @@ module Interferon::Destinations
 
       datadog_query = alert['metric']['datadog_query'].strip
       existing_alert = existing_alerts[alert['name']]
-
-      if @datadog_query_parser.parse(datadog_query.split.join('')).nil?
-        log.warn "Invalid datadog query in #{alert['name']}: #{datadog_query} #{@datadog_query_parser.failure_reason}"
-      end
 
       # new alert, create it
       if existing_alert.nil?
