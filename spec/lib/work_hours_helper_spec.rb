3301c0a621da2adc7fc6a55cf579de4ab0d3036c
Jimmy Ngo <jimmy.ngo@airbnb.com>
2018-05-15 11:55:56 -0700

    [host_sources] Skip tables that do not exist
    The current host_source for aws_dynamo will error out if it cannot get
    the read/write capacity for the dynamo table. This can occur if the
    table is being restored.

--- spec/lib/work_hours_helper_spec.rb
+++ spec/lib/work_hours_helper_spec.rb
@@ -9,34 +9,46 @@ describe Interferon::WorkHoursHelper do
   describe '.now_is_work_hour?' do
     context 'when it is a work hour in a work day' do
       it 'return true' do
-        expect(subject.is_work_hour?(
-                 Time.parse('Mon Nov 26 9:01:20 PST 2001').utc
-        )).to be_truthy
-        expect(subject.is_work_hour?(
-                 Time.parse('Fri Nov 30 16:35:20 PST 2001').utc
-        )).to be_truthy
+        expect(
+          subject.is_work_hour?(
+            Time.parse('Mon Nov 26 9:01:20 PST 2001').utc
+          )
+        ).to be_truthy
+        expect(
+          subject.is_work_hour?(
+            Time.parse('Fri Nov 30 16:35:20 PST 2001').utc
+          )
+        ).to be_truthy
       end
     end
 
     context 'when it is a work hour in a weekend' do
       it 'return false' do
-        expect(subject.is_work_hour?(
-                 Time.parse('Sat Nov 24 09:01:20 PST 2001').utc
-        )).to be_falsy
-        expect(subject.is_work_hour?(
-                 Time.parse('Sun Nov 25 09:01:20 PST 2001').utc
-        )).to be_falsy
+        expect(
+          subject.is_work_hour?(
+            Time.parse('Sat Nov 24 09:01:20 PST 2001').utc
+          )
+        ).to be_falsy
+        expect(
+          subject.is_work_hour?(
+            Time.parse('Sun Nov 25 09:01:20 PST 2001').utc
+          )
+        ).to be_falsy
       end
     end
 
     context 'when it is not a work hour' do
       it 'return false' do
-        expect(subject.is_work_hour?(
-                 Time.parse('Thu Nov 29 08:33:20 PST 2001').utc
-        )).to be_falsy
-        expect(subject.is_work_hour?(
-                 Time.parse('Fri Nov 30 17:33:20 PST 2001').utc
-        )).to be_falsy
+        expect(
+          subject.is_work_hour?(
+            Time.parse('Thu Nov 29 08:33:20 PST 2001').utc
+          )
+        ).to be_falsy
+        expect(
+          subject.is_work_hour?(
+            Time.parse('Fri Nov 30 17:33:20 PST 2001').utc
+          )
+        ).to be_falsy
       end
     end
   end
